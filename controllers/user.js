26517ed62f549b9eeaa0f8f478d5a0d4dc1ccaf0
Naveen5676 <sample.first@yahoo.com>
2024-04-22 23:11:23 +0530

    created login api

--- controllers/user.js
+++ controllers/user.js
@@ -19,3 +19,26 @@ exports.signup = (req, res, next) => {
       res.status(500).send(err);
     });
 };
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  usermodel
+    .findAll({where :{email :email}})
+    .then((users) => {
+      if (users.length == 0) {
+        return res.status(404).json({ error: "Email not found" });
+      }
+      const user = users[0]
+      if (user.password === password) {
+        return res.status(200).json({ message: "success" });
+      } else {
+        return res.status(401).json({ error: "Password does not match" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({error:'server error'})
+    });
+};
