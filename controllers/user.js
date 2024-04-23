1c60cade6270425beacd81b6d41c6f25074c204c
Naveen5676 <sample.first@yahoo.com>
2024-04-23 14:05:31 +0530

    add encryption to password

--- controllers/user.js
+++ controllers/user.js
@@ -1,23 +1,27 @@
 const usermodel = require("../models/user");
+const bcrypt = require("bcrypt");
 
 exports.signup = (req, res, next) => {
   let name = req.body.name;
   let email = req.body.email;
   let password = req.body.password;
 
-  usermodel
-    .create({
-      name: name,
-      email: email,
-      password: password,
-    })
-    .then((response) => {
-      res.status(200).json(response);
-    })
-    .catch((err) => {
+  bcrypt.hash(password, 10, async (err, hash) => {
+    if (err) {
+      throw new Error("Something went wrong while hashing a password");
+    }
+    try {
+      let user = await usermodel.create({
+        name: name,
+        email: email,
+        password: hash,
+      });
+      res.status(200).json(user);
+    } catch (err) {
       console.log(err);
       res.status(500).send(err);
-    });
+    }
+  });
 };
 
 exports.login = (req, res, next) => {
@@ -25,20 +29,22 @@ exports.login = (req, res, next) => {
   const password = req.body.password;
 
   usermodel
-    .findAll({where :{email :email}})
+    .findAll({ where: { email: email } })
     .then((users) => {
       if (users.length == 0) {
-        return res.status(404).json({ error: "Email not found" });
+        throw new Error('could  not find any users with this email');
       }
-      const user = users[0]
-      if (user.password === password) {
-        return res.status(200).json({ message: "success" });
-      } else {
-        return res.status(401).json({ error: "Password does not match" });
-      }
+      const user = users[0];
+      bcrypt.compare(password, user.password, (error, result) => {
+        if (!error) {
+          return res.status(200).json({ message: "success" });
+        } else {
+          return res.status(401).json({ error: "Password does not match" });
+        }
+      });
     })
     .catch((err) => {
       console.log(err);
-      res.status(500).json({error:'server error'})
+      res.status(500).json({ error: err });
     });
 };
