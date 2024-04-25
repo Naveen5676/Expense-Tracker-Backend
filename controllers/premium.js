ccd363986f6588a7671f267f632eb056ae297324
Naveen5676 <sample.first@yahoo.com>
2024-04-25 19:01:37 +0530

    Leaderboard Optimisation

--- controllers/premium.js
+++ controllers/premium.js
@@ -1,29 +1,26 @@
 const Sequelize = require("sequelize");
 const expensemodel = require("../models/expense");
 const usermodel = require("../models/user");
+const sequelize = require("../utils/database");
+const { compareSync } = require("bcrypt");
 
 exports.getuserleaderhipBoard = async (req, res, next) => {
   try {
-    const users = await usermodel.findAll();
-    const expenses = await expensemodel.findAll();
-
-    const userAggregatedExpenses = {};
-
-    expenses.forEach((expense) => {
-      if (userAggregatedExpenses[expense.userdatumId]) {
-        userAggregatedExpenses[expense.userdatumId] += expense.expenseamt;
-      } else {
-        userAggregatedExpenses[expense.userdatumId] = expense.expenseamt;
-      }
+    const leaderboardofuser = await usermodel.findAll({
+      attributes: [
+        'id',
+        'name',
+        [sequelize.fn('sum', sequelize.col('expensedata.expenseamt')), 'total_cost'] // first sum is the operation and get they col and then total_cost is they alias name
+      ],
+      include: [{
+        model: expensemodel,
+        attributes: []
+      }],
+      group: ['userdata.id'] ,// Include all non-aggregated columns in GROUP BY
+      order:[[sequelize.col('total_cost') , "DESC"]] // in order to display maximum first or in descending order
     });
-    const userleaderboarddetails=[];
-    users.forEach((user)=>{
-        userleaderboarddetails.push({name: user.name , total_cost : userAggregatedExpenses[user.id] || 0 })
 
-    })
-    //console.log(userAggregatedExpenses);
-    userleaderboarddetails.sort((a,b)=> b.total_cost - a.total_cost)
-    res.status(200).json(userleaderboarddetails);
+    res.status(200).json(leaderboardofuser);
   } catch (err) {
     console.error(err);
   }
